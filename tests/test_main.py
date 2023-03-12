ec0a9e2379ed6e1f8602ebc37909382c711ac9ab
Alexei <prog_alexei@mail.ru>
2023-03-12 11:09:03 +0300

    Added tests. Home_work_3 completed.

--- tests/test_main.py
+++ tests/test_main.py
@@ -29,3 +29,18 @@ def test__repr(rnd_item):
 def test_item_name(rnd_item):
     assert rnd_item.item_name == 'Телевизор'
 
+
+def test_item__add():
+    item = Item("iPhone 14", 120_000, 5)
+    phone = Phone("iPhone 14", 120_000, 10, 2)
+    assert (item + phone) == 15
+
+
+def test_phone_init():
+    phone = Phone("iPhone 14", 120_000, 10, 2)
+    assert phone.price == 120000
+
+
+def test_phone_num_sims():
+    phone = Phone("iPhone 14", 120_000, 10, 2)
+    assert phone.num_sims == 2
